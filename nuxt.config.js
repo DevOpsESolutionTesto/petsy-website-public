3a8d4ad8eaf078dfa61e66aacebd49591111ea11
Jonas Einsele <jeinsele@testo.de>
2022-05-23 10:21:46 +0200

    change title of page

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'mywebsite',
+    title: 'Petsy',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
