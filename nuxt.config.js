686ff4e18acf7420ae115d479f690c2bc22785fd
Jonas Einsele <jeinsele@testo.de>
2022-05-23 10:09:40 +0200

    env port for heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ const StoryblokToken = '***REMOVED***'
 
 module.exports = {
   mode: 'spa',
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  },
   modules: [
     ['storyblok-nuxt', {accessToken: StoryblokToken, cacheProvider: 'memory'}]
   ],
