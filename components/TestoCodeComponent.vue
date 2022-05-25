c06f1a6f5404defa656c1a058ee715aa8620eeb0
Jonas Einsele <jeinsele@testo.de>
2022-05-25 16:24:22 +0200

    fix loading of booking script

--- components/TestoCodeComponent.vue
+++ components/TestoCodeComponent.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
     <div :id="blok.divWrapId">
-      <script>
-        window.onload = function () {
-          {{blok.code.content[0].content[0].text}}
-        };
-      </script>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["blok"],
-  mounted() {
-    let Script = document.createElement("script");
-    Script.setAttribute("src", this.blok.url);
-    Script.defer = true;
-    document.head.appendChild(Script);
-  },
-};
+  props: ['blok'],
+  mounted () {
+    let Script = document.createElement('script')
+    Script.setAttribute('src', this.blok.url)
+    Script.defer = true
+    Script.addEventListener('load', () => {
+      // eslint-disable-next-line no-undef
+      cmscontent()
+    })
+
+    document.head.appendChild(Script)
+    let ScriptCms = document.createElement('script')
+    ScriptCms.text = 'function cmscontent(){' + this.blok.code.content[0].content[0].text + '}'
+
+    document.head.appendChild(ScriptCms)
+  }
+}
 </script>
